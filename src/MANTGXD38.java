c6e3c464f69dbb3c4275c105688482ec15a919fe
oscarvid <o.j.arvidson@hotmail.com>
2019-11-27 10:10:15 +0100

    ye

---
+++ src/MANTGXD38.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+/**
+ * @author Oscar Arvidson and Erik Wetter
+ * Subclass of CarCarrierTruck
+ */
+
+public class MANTGXD38 extends CarCarrierTruck {
+
+    public MANTGXD38() {
+        super(Direction.NORTH, 200, 100, 2, 593, Color.lightGray, "MAN TGX D38", 2.5, 3, 18, 6, 2.20, 2, 5);
+    }
+}
---
+++ src/Scania.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+/**
+ * @author Oscar Arvidson and Erik Wetter
+ * Subclass of FlatbedTruck
+ */
+public class Scania extends FlatbedTruck {
+
+    /**
+     *
+     */
+    public Scania() {
+        super(Direction.NORTH, 10,30, 2, 500, Color.cyan, "ScaniaG530", 2.5, 5, 10, 70);
+    }
+}
